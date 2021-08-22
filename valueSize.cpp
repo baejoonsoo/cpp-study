03e14b7496c86fd6d5765cf3e92faaeda5dea89d
baejoonsoo <baejoonsoo21@gmail.com>
2021-08-22 18:08:40 +0900

    value size

---
+++ valueSize.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+
+int main(){
+    std::cout<<"short : "<<sizeof(short)<<std::endl;
+    std::cout<<"int : "<<sizeof(int)<<std::endl;
+    std::cout<<"long int : "<<sizeof(long int)<<std::endl;
+    std::cout<<"long long int : "<<sizeof(long long int)<<std::endl;
+    std::cout<<"char : "<<sizeof(char)<<std::endl;
+    std::cout<<"float : "<<sizeof(float)<<std::endl;
+    std::cout<<"double : "<<sizeof(double)<<std::endl;
+    std::cout<<"long double : "<<sizeof(long double)<<std::endl;
+
+}
