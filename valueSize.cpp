7b292120155cdd206ac5a1bba2ec167bf44dbf05
baejoonsoo <baejoonsoo21@gmail.com>
2021-08-22 21:48:37 +0900

    value size comment

--- valueSize.cpp
+++ valueSize.cpp
@@ -11,4 +11,12 @@ int main(){
     std::cout<<"double : "<<sizeof(double)<<std::endl;
     std::cout<<"long double : "<<sizeof(long double)<<std::endl;
 
+    // short : 2
+    // int : 4
+    // long int : 8
+    // long long int : 8
+    // char : 1
+    // float : 4
+    // double : 8
+    // long double : 16
 }
