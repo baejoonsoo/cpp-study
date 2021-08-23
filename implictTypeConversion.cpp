d1aab1fa3cf6ad7a90acf1a2e0df6d1b4919f355
baejoonsoo <baejoonsoo21@gmail.com>
2021-08-23 20:04:26 +0900

    implict Type Conversion

--- implictTypeConversion.cpp
+++ implictTypeConversion.cpp
@@ -17,5 +17,13 @@ int main(){
 
     //numberic conversion
     //숫자 변환으로 인해 데이터가 손실되거나 그러지 않을 수도 있다.
+
+    //범위가 충분히 크지 않은 자료형으로 변환하면 예기치 않는 결과가 발생한다.
     std::cout<<"c : "<<"\" "<<c<<" \""<<" = "<< static_cast<int>(c)<<std::endl;
+
+    float f=3.14f;
+    int num=f;
+
+    //부동 소수점 숫자에서 정수로 변환하는 것은 분수 값을 모두 손실시킨다.
+    std::cout<<"3.14 : "<<num<<std::endl;
 }
